dd1f1761474f841d6431e5206dc4d383270c9708
su1107 <535355594@qq.con>
2016-05-28 09:59:09 +0800

    第一次测试

---
+++ susu/src/susu/test.java
@@ -0,0 +1,10 @@
+package susu;
+
+import java.awt.peer.SystemTrayPeer;
+import java.security.Principal;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("jghig");
+	}
+}
